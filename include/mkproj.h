581b81fa7c63f108e0ea6495f0e2e39245f5d962
Daniel Kim <dkim311006@gmail.com>
2026-08-30 23:37:22 +0900

    Update mkproj.h

--- include/mkproj.h
+++ include/mkproj.h
@@ -66,9 +66,9 @@ typedef struct {
 
 // == FUNCTIONS ===========================================================
   /* -- flag options ---------------------------------------------------- */
-extern int option_default(const char *);
-extern int option_bare(const char *);
-extern int option_full(const char *);
+extern int mkproj_option_default(const char *);
+extern int mkproj_option_bare(const char *);
+extern int mkproj_option_full(const char *);
   /* ---------------------------------------------------- flag options -- */
 
   /* -- populate files -------------------------------------------------- */
